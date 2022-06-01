eb04eed50223d1af0e0aa6b94411992ad25d83d7
Dmitry <dmitrytxt@gmail.com>
2022-06-01 23:36:00 +0300

    city c1

--- src/test/java/AviaSalesTest.java
+++ src/test/java/AviaSalesTest.java
@@ -14,7 +14,7 @@ public class AviaSalesTest {
         driver = new ChromeDriver();
         driver.get(System.getProperty("url"));
         WebElement inpDestination = driver.findElement(By.xpath("//input[@id='destination']"));
-        inpDestination.sendKeys("Talin");
+        inpDestination.sendKeys("Omsk");
         WebElement btnChangeDirection = driver.findElement(By.xpath("//div[@class='swap-places']"));
         btnChangeDirection.click();
         driver.quit();
